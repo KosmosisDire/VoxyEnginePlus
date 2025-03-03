2c05b899d22f9fe8e703ccd7297dc8876ae7e32d
Nathan George <kosmosisdire@gmail.com>
2025-03-03 03:09:35 -0500

    [WIP] misc lighting improvments, small refactor hashmap

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include "voxel-app.hpp"
+#include <scripting/example.hpp>
 
 auto main() -> int
 {
     VoxyApp app;
-    // GameWindow window = GameWindow("App", 1920, 1080);
     while (true)
     {
-        // window.Update();
         if (app.Update())
         {
             break;
         }
     }
+
     return 0;
 }
