4d228774d21e49a9530bf658476c4f8597fc0105
Nathan George <kosmosisdire@gmail.com>
2025-04-25 03:06:34 -0400

    [WIP] Build is working fully, but removed angelscript

--- PrismEngine/src/main.cpp
+++ PrismEngine/src/main.cpp
@@ -1,23 +1,13 @@
-#include "project_paths.h"
+#include <project_paths.h>
 #include "voxel-app.hpp"
 #include <engine/apis/Resources.hpp>
-#include <scripting/scripting.hpp>
-#include <iostream> // For test output
 
 auto main() -> int
 {
-    scriptingEngine.registerStdLib();
-    scriptingEngine.registerEngineAPI();
-
     VoxyApp app;
-
-    scripts->addScriptFile(Resources::GetResourcePath("test.as").string());
-    scriptingEngine.savePredefinedScript(PROJECT_ROOT_DIR "/as.predefined");
 
     while (true)
     {
-        scriptingEngine.checkHotReload();
-
         if (app.Update())
         {
             break;
